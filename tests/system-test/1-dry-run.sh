a5202ddb726d35a5a191d62332595d8eb370c031
Jean Honlet <j.honlet@evs.com>
2019-03-22 21:08:18 +0100

    chore: add titles in display

--- tests/system-test/1-dry-run.sh
+++ tests/system-test/1-dry-run.sh
@@ -8,15 +8,23 @@ setup "$T"
 
 checkConsistency
 
+
+log_info "## help"
+
 runItAndCapture "help" --help
 assert_captured_success
+
+
+log_info "## Incorrect options"
 
 runItAndCapture "incorrect arguments" -blabla
 assert_true "Should be in error" "$( [[ $CAPTURED_EXITCODE -gt 0 ]] )"
 
+
+log_info "## Dry run"
+
 runItAndCapture "dry-run" -n
 assert_captured_success
-
 capture "No file are modified" rsync -r --delete "$ORIG_DATA" "$TEST_DATA"
 assert_equal "No file are modified" "" "$CAPTURED_OUTPUT"
 
