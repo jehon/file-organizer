e59b9abcf557df22e57c2e775a8f6d2d4480b8a2
Jean Honlet <jehon@users.noreply.github.com>
2019-09-20 12:37:17 +0200

    Implement some tests on movie

--- test/unit/file-movie-test.js
+++ test/unit/file-movie-test.js
@@ -2,6 +2,7 @@
 const options = require('../../file-organizer/options.js');
 const { dataPath, createFileGeneric } = require('./helpers.js');
 const FileMovie = require('../../file-organizer/file-movie.js');
+const FileFactory = require('../../file-organizer/file-factory.js');
 
 const { tsFromString } = require('../../file-organizer/timestamp.js');
 
@@ -9,57 +10,45 @@ async function getMov(dPath) {
 	return new FileMovie(dataPath(dPath)).loadData();
 }
 
-const AndroidMP4 = '2019-09-03 12-48/20190903_124726.mp4';
+const canonMOV = 'DSC_2506.MOV';
+const canonMOVTS = '2019-09-19 07-48-25';
 
 describe('file-movie-test', () => {
 	it('should get exiv from files', async () => {
 		// Adroid files
-		expect((await getMov(AndroidMP4)).exiv_timestamp.TS()).toBe('2019-09-03 12-47-31');
+		expect((await getMov(canonMOV)).exiv_timestamp.TS()).toBe(canonMOVTS);
 	});
 
 	it('should get comment from files', async () => {
 		// Android files
-		expect((await getMov(AndroidMP4)).exiv_comment).toBe('');
+		expect((await getMov(canonMOV)).exiv_comment).toBe('');
 	});
 
-	xit('should write timestamps correctly', async() =>  {
-		const new1 = await createFileGeneric(AndroidMP4);
-		expect(new1.exiv_timestamp.TS()).toBe('2019-09-03 12-47-31');
+	it('should write timestamps correctly', async() =>  {
+		const new1 = await createFileGeneric(canonMOV);
+		expect(new1.exiv_timestamp.TS()).toBe(canonMOVTS);
 
-		new1.exivWriteTimestamp('2016-02-04 01-02-03');
+		await new1.exivWriteTimestamp('2016-02-04 01-02-03');
 		expect(new1.exiv_timestamp.TS()).toBe('2016-02-04 01-02-03');
 
-		const new2 = await createFileGeneric(AndroidMP4);
+		const new2 = await FileFactory(new1.getRelativePath()).loadData();
 		expect(new2.exiv_timestamp.TS()).toBe('2016-02-04 01-02-03');
 
 		new1.remove();
 	});
 
-	describe('check', () => {
-		xit('should be problems when no exiv is present', async() => {
-			const new1 = await getMov('no_exiv.jpg');
-			await new1.check();
-			expect(new1.errors).toContain('EXIV_NO_DATE');
-		});
+	it('should write comments correctly', async() =>  {
+		const newComment = 'test';
+		const new1 = await createFileGeneric(canonMOV);
+		expect(new1.exiv_comment).toBe('');
 
-		xit('should set comment if necessary', async() => {
-			options.guessComment = true;
+		await new1.exivWriteComment(newComment);
+		expect(new1.exiv_comment).toBe(newComment);
 
-			// const new1 = await createFileGeneric('no_exiv.jpg');
-			// expect(new1.exiv_comment).toBe('');
-			// expect(new1.exiv_timestamp.TS()).toBe('');
+		const new2 = await FileFactory(new1.getRelativePath()).loadData();
+		expect(new2.exiv_comment).toBe(newComment);
 
-			// new1.exiv_timestamp = tsFromString('2018-01-02');
-
-			// new1.calculatedTS.year = 2018;
-			// new1.calculatedTS.comment = 'override comment';
-
-			// await new1.check();
-			// expect(new1.errors).toContain('EXIV_WRITE_COMMENT');
-			// expect(new1.exiv_comment).toBe('override comment');
-			// new1.remove();
-
-			options.resetToDefault();
-		});
+		new1.remove();
 	});
+
 });
