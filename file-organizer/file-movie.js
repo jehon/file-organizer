ec9673e6a3b9fc4ab986c59b2401ad0662a5d050
Jean Honlet <jehon@users.noreply.github.com>
2019-09-20 12:37:17 +0200

    Implement some tests on movie

--- file-organizer/file-movie.js
+++ file-organizer/file-movie.js
@@ -1,27 +1,8 @@
 
 const FileExiv = require('./file-exiv.js');
-const { tzFromGPS, tsFromDateAndTimezone } = require('./timestamp.js');
-
 
 module.exports = class FileMovie extends FileExiv {
 	getType() {
 		return 'movie';
-	}
-	// TODO: hook data functions
-
-	exivReadAll(file) {
-		const resultObj = super.exivReadAll(file);
-		// console.log(resultObj);
-		if (!resultObj.DateTimeOriginal && resultObj.CreateDate) {
-			// CreateDate
-			// GPSPosition
-			if (resultObj.GPSPosition) {
-				const tz = tzFromGPS(resultObj.GPSPosition);
-				resultObj.DateTimeOriginal = tsFromDateAndTimezone(resultObj.CreateDate.replace(':', '-').replace(':', '-'), tz).TS();
-			} else {
-				resultObj.DateTimeOriginal = resultObj.CreateDate;
-			}
-		}
-		return resultObj;
 	}
 };
