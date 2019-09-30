ff9a0b3a5ffff613877bb1e302acd56528c6a18e
Jean Honlet <jehon@users.noreply.github.com>
2019-09-30 21:14:58 +0200

    rework: use spawn instead of spawnsync
    - item
    - item

--- file-organizer/file-utils.js
+++ file-organizer/file-utils.js
@@ -1,5 +1,5 @@
 
-var spawnSync = require('child_process').spawnSync;
+const spawn = require('spawn-promise');
 
 const fs = require('fs-extra');
 
@@ -18,35 +18,30 @@ async function fileRename(filePathOriginal, filePathDest) {
 
 	try {
 		if (filePathOriginal.toUpperCase() == filePathDest.toUpperCase()) {
-			await fileRename(filePathOriginal, filePathOriginal + '.case');
-			await fileRename(filePathOriginal + '.case', filePathDest);
+			return fileRename(filePathOriginal, filePathOriginal + '.case')
+				.then(() => fileRename(filePathOriginal + '.case', filePathDest))
+				.then(() => true);
 		} else {
 			if (await fileExists(filePathDest)) {
 				throw new Error(`A file with the same name already exists (${filePathDest} from ${filePathOriginal})`);
 			}
-			spawnSync('mv', [ filePathOriginal, filePathDest ]).stdout.toString();
+			return fileExec('mv', [ filePathOriginal, filePathDest ])
+				// .then(buffer => console.log(buffer.toString()))
+				.then(() => true);
 		}
-		return true;
 	} catch (e) {
 		throw Error('Error in fileRename: ', e);
 	}
 }
 
-async function fileExec(file, params = [], options = {}) {
-	// try {
-	const res = spawnSync(file, params, Object.assign({
-		stdio: [ 'ignore', 'pipe', 'inherit' ]
-	}, options));
-	if (res.error) {
-		throw res.error;
-	}
-	if (res.status > 0) {
-		throw Error(`Process ${file} returned result ${res.status}`);
-	}
-	if (res.stdout) {
-		return res.stdout.toString();
-	}
-	return '';
+async function fileExec(file, params = []) {
+	return spawn(file, params)
+		.catch(res => {
+			// console.error(res);
+			throw Error(res);
+		})
+		.then(logBuffer => logBuffer ? logBuffer.toString() : '')
+	;
 }
 
 module.exports = {
