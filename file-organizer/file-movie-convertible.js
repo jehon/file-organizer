f5690e762b6ed0e0c33926b67b0ea79d94f01aa3
Jean Honlet <jehon@users.noreply.github.com>
2019-10-10 15:25:30 +0200

    WIP: file-movie-convertible

--- file-organizer/file-movie-convertible.js
+++ file-organizer/file-movie-convertible.js
@@ -1,15 +1,27 @@
 
+const fileUtils = require('./file-utils.js');
 const FileMovie = require('./file-movie.js');
 
-module.exports = class FileMovieConvertible extends FileMovie {
-	getType() {
-		return 'movie';
-	}
+function buildTargetName(filepath, newExtension) {
+	return fileUtils.getDirname(filepath) + fileUtils.getFilename(filepath) + newExtension;
+}
 
-	async check() {
+function convertAbstract(targetExtension, command, targetClass) {
+
+}
+
+function convertToMov(filepath) {
+	return convertAbstract('.mov',
+		() =>
 		// ffmpeg -i <name>.avi <name>.mov
-		// exiftool -TagsFromFile <name>.avi <name>.mov
+			fileUtils.fileExec('ffmpeg', '-i', filepath, buildTargetName(filepath, '.mov'))
+			// exiftool -TagsFromFile <name>.avi <name>.mov
+				.then(() => fileUtils.fileExec('exiftool', '-TagsFromFile', filepath, buildTargetName(filepath, '.mov'))),
+		FileMovie
+	);
+}
 
-		return super.check();
-	}
+module.exports = {
+	convertAbstract,
+	convertToMov
 };
