01a534ff5633bc3ec7c3bffe6046ba519a125002
Jean Honlet <jehon@users.noreply.github.com>
2019-10-15 19:07:33 +0200

    doc: Add doc on exiv

--- file-organizer/file-exiv.js
+++ file-organizer/file-exiv.js
@@ -4,6 +4,13 @@
  *
  * See https://www.sno.phy.queensu.ca/~phil/exiftool/#supported
  *
+ *
+ * According to the specification, many QuickTime date/time tags should be stored as UTC.
+ * Unfortunately, digital cameras often store local time values instead (presumably because
+ * they don't know the time zone). For this reason, by default ExifTool does not assume
+ * a time zone for these values. However, if the QuickTimeUTC API option is set, then ExifTool
+ * will assume these values are properly stored as UTC, and will convert them to local time when extracting.
+ *
  */
 
 const FileTimestamped = require('./file-timestamped.js');
