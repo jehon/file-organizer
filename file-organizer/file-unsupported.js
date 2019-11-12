449dd5c8f70f77e0770d236f8a78a5fc118b07b7
Jean Honlet <jehon@users.noreply.github.com>
2019-11-12 21:08:31 +0100

    feature: unsupported seen as impossible

--- file-organizer/file-unsupported.js
+++ file-organizer/file-unsupported.js
@@ -1,13 +1,17 @@
 
-const FileHidden = require('./file-hidden.js');
+const FileGeneric = require('./file-generic.js');
 
 const map = new Map();
 
-class FileUnsupported extends FileHidden {
+class FileUnsupported extends FileGeneric {
 	constructor(filePath, parent = null) {
 		super(filePath, parent);
 		const i = map.has(this.getExtension().toLowerCase()) ? map.get(this.getExtension().toLowerCase()) : 0;
 		map.set(this.getExtension().toLowerCase(), i + 1);
+	}
+
+	async check() {
+		return this.addMessageImpossible('FILE_UNSUPPORTED', 'File unuspported: ' + this.getExtension());
 	}
 }
 
