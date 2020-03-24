fbc7cd1cfd469c11c2a118b8712b660e78842018
Jean Honlet <jehon@users.noreply.github.com>
2020-03-24 20:06:13 +0100

    wip: affine listener

--- file-organizer/renderer/listener.js
+++ file-organizer/renderer/listener.js
@@ -3,6 +3,6 @@ const ipcRenderer = require('electron').ipcRenderer;
 
 module.exports = function (channel, cb) {
     ipcRenderer.on(channel, (_event, data) => {
-        cb(data);
+        cb(data.type, data);
     });
 }
