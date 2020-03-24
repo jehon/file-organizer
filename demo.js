96d27fec8b0e1670ee84aa1b87e9df87a37e5a8b
Jean Honlet <jehon@users.noreply.github.com>
2020-03-24 15:05:23 +0100

    wip: demo is normal page, but main (driver) is different

--- demo.js
+++ demo.js
@@ -1,23 +1,10 @@
 
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
 const options = require('./file-organizer/options.js');
 const { notify } = require('./file-organizer/messenger.js');
+const constants = require('./file-organizer/constants.js');
 
 options.headless = false;
 
-function send(channel, data) {
-    BrowserWindow.getAllWindows().forEach(b => b.webContents.send(channel, data));
-}
+require('./file-organizer/gui.js');
 
-app.whenReady()
-    .then(() => {
-        const mainWindow = new BrowserWindow();
-        mainWindow.loadFile(path.join(__dirname, 'file-organizer/renderer/demo.html'));
-        mainWindow.webContents.on('dom-ready', () => {
-            // TODO: here
-
-
-        });
-    });
+notify('hello world', {})
