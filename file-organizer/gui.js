cc6ee9dc4b3edb58dcf7dd4d6009dd7d7d38fa15
Jean Honlet <jehon@users.noreply.github.com>
2020-03-24 20:45:41 +0100

    wip: has a 80% of screen

--- file-organizer/gui.js
+++ file-organizer/gui.js
@@ -1,6 +1,6 @@
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
 const { register } = require('./messenger.js');
@@ -12,7 +12,12 @@ function onEvent(channel, data) {
 if (app) {
     app.whenReady()
         .then(() => {
+            const { width, height } = screen.getPrimaryDisplay().workAreaSize
+            console.log(width, height);
+
             const mainWindow = new BrowserWindow({
+                width: Math.floor(parseInt(width) * 0.8),
+                height: Math.floor(parseInt(height) * 0.8),
                 webPreferences: {
                     nodeIntegration: true
                 }
