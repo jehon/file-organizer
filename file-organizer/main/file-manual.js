a0968a9b9f1b8e6f99d7ed344208480ac1be7e36
Jean Honlet <jehon@users.noreply.github.com>
2020-04-15 22:05:28 +0200

    style: eslint

--- file-organizer/main/file-manual.js
+++ file-organizer/main/file-manual.js
@@ -9,6 +9,6 @@ module.exports = class FileManual extends File {
     async analyse() {
         // return this.addMessageImpossible('TS_MANUAL', 'Manual operation needed');
         return super.analyse()
-            .then(() => this.notify(STATUS_FAILURE))
+            .then(() => this.notify(STATUS_FAILURE));
     }
 };
