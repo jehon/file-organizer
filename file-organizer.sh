305aa87fd6458f6f496f49dd98ae938399225f29
Jean Honlet <jehon@users.noreply.github.com>
2020-11-09 21:44:56 +0100

    feature:

--- file-organizer.sh
+++ file-organizer.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 # set -x
 
 SWD="$( dirname "$0")"
 # echo "SWD: $SWD"
 
-"$SWD"/node_modules/.bin/electron "$SWD"/file-organizer/main.js "$@"
+"$SWD"/node_modules/.bin/electron "$SWD"/src/main.cjs "$@" |& grep -v "ExperimentalWarning"
---
+++ src/main.cjs
@@ -0,0 +1,2 @@
+
+import('../file-organizer/main.js');
