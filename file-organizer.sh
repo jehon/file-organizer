b73ce32bd518ca975995f25acb246977da76be19
Jean Honlet <jehon@users.noreply.github.com>
2020-11-13 15:18:36 +0100

    chore: filter message from node

--- file-organizer.sh
+++ file-organizer.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # set -e
-# set -o pipefail
+set -o pipefail
 
 SWD="$( dirname "$0")"
 
-"$(npm bin)"/electron "$SWD" "$@"
+# "$(npm bin)"/electron "$SWD" "$@"
+
+"$(npm bin)"/electron "$SWD" "$@" 3>&1 1>&2 2>&3 | ( grep -v "ExperimentalWarning" || true ) 3>&1 1>&2 2>&3
+
