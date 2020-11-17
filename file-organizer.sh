a49a083a1ea479851bff6481fcf9ce83b5e2e751
Jean Honlet <jehon@users.noreply.github.com>
2020-11-17 13:52:33 +0100

    feature: run from anywhere

--- file-organizer.sh
+++ file-organizer.sh
@@ -3,8 +3,6 @@
 # set -e
 set -o pipefail
 
-SWD="$( dirname "$0")"
+SWD="$( dirname "$( realpath -P "${BASH_SOURCE[0]}" )" )"
 
-# "$(npm bin)"/electron "$SWD" "$@"
-
-( "$(npm bin)"/electron "$SWD" "$@" 3>&1 1>&2 2>&3 | ( grep -v "ExperimentalWarning" || true )) 3>&1 1>&2 2>&3
+( "$SWD/node_modules/.bin/electron" "$SWD" "$@" 3>&1 1>&2 2>&3 | ( grep -v "ExperimentalWarning" || true )) 3>&1 1>&2 2>&3
