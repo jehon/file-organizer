41c80cd44ad966410bc0b8bc95607d58b6f07225
Jean Honlet <jehon@users.noreply.github.com>
2020-12-01 21:24:23 +0100

    chore: simplify file-organier-headless.sh

--- file-organizer-headless.sh
+++ file-organizer-headless.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 SWD="$( dirname "$( realpath -P "${BASH_SOURCE[0]}" )" )"
 
-set -o pipefail
-( "$SWD/src/main/main.js" "$@" 3>&1 1>&2 2>&3 | ( grep -v "ExperimentalWarning" || true )) 3>&1 1>&2 2>&3
-
-# set -e
-# "$SWD/src/main/main.js" "$@"
+"$SWD/src/main/main.js" "$@"
