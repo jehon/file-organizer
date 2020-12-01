6ed55ee04ae8f58dba16ec38053c523b946fb665
Jean Honlet <jehon@users.noreply.github.com>
2020-12-01 12:10:09 +0100

    feature: try catch when loading CJS

--- src/main/loadFileTypes.js
+++ src/main/loadFileTypes.js
@@ -11,15 +11,21 @@ export default async function loadFileTypes() {
     // We could not reset, because in esm modules, the init
     // is done on first load
     //
-
-    const loadCJS = (f) => import(f).then(ft => ft.default.init());
+    const loadCJS = function (f) {
+        return import('../../file-organizer/' + f)
+            .then(ft => ft.default.init())
+            .catch(e => {
+                console.error(`Error loading ${f}`, e);
+                throw e;
+            });
+    };
 
     await Promise.all(
         [
-            loadCJS('../../file-organizer/file-generic.js'),
-            loadCJS('../../file-organizer/file-folder.js'),
-            loadCJS('../../file-organizer/file-movie.js'),
-            loadCJS('../../file-organizer/file-picture.js'),
+            loadCJS('file-generic.js'),
+            loadCJS('file-folder.js'),
+            loadCJS('file-movie.js'),
+            loadCJS('file-picture.js'),
         ]);
 
     await importDirectory('src/main/file-types', /file-.+[.]js/);
