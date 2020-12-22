854a65919d1815cbba26f902076069d1439c29ba
Jean Honlet <jehon@users.noreply.github.com>
2020-12-22 09:01:19 +0100

    docs: fix comment

--- src/main/file-types/file.js
+++ src/main/file-types/file.js
@@ -374,7 +374,7 @@ export default class File extends Item {
     // }
 
     /**
-     * Run the analysis on this files
+     * Run the analysis on this files, and all related one's (ex: FileFolder)
      *
      * @private
      *
