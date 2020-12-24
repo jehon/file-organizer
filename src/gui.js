aebec7bb45e2dfe7ee994a2b2109d67ca325a153
Jean Honlet <jehon@users.noreply.github.com>
2020-12-24 21:37:31 +0100

    refactor: gui

--- src/gui.js
+++ src/gui.js
@@ -18,44 +18,40 @@ import options from './common/options.js';
 
 import { rootDir } from './main/main-constants.js';
 
-export default new Promise((resolve, _reject) => {
-    if (app) {
-        app.whenReady()
-            .then(() => {
-                console.info('App is ready');
-                const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
-                const mainWindow = new BrowserWindow({
-                    width: Math.min(1024, Math.floor(parseInt('' + width) * 0.8)),
-                    height: Math.min(800, Math.floor(parseInt('' + height) * 0.8)),
-                    webPreferences: {
-                        nodeIntegration: true,
-                        contextIsolation: false
-                    }
-                });
-
-                if (options.debug) {
-                    mainWindow.webContents.openDevTools();
-                }
-
-                console.info('waiting for dom ready');
-                mainWindow.loadFile(path.join(rootDir, 'src/renderer/index.html'))
-                    .then(() => {
-                        registerGuiCallback((data) => {
-                            BrowserWindow.getAllWindows().forEach(b => b.webContents.send(CHANNEL_MAIN, data));
-                        });
-                        resolve();
-                    });
-
-                // Close (exit) when all windows are closed
-                app.on('window-all-closed', function () {
-                    if (!options.headless) {
-                        app.quit();
-                    }
-                });
-            });
-    } else {
+(async () => {
+    if (!app) {
         console.info('No app found, not launching gui');
-        resolve();
+        return;
     }
-});
+
+    await app.whenReady();
+
+    console.info('App is ready');
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+    const mainWindow = new BrowserWindow({
+        width: Math.min(1024, Math.floor(parseInt('' + width) * 0.8)),
+        height: Math.min(800, Math.floor(parseInt('' + height) * 0.8)),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    if (options.debug) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    console.info('waiting for dom ready');
+    await mainWindow.loadFile(path.join(rootDir, 'src/renderer/index.html'));
+    registerGuiCallback((data) => {
+        BrowserWindow.getAllWindows().forEach(b => b.webContents.send(CHANNEL_MAIN, data));
+    });
+
+    // Close (exit) when all windows are closed
+    app.on('window-all-closed', function () {
+        if (!options.headless) {
+            app.quit();
+        }
+    });
+})();
