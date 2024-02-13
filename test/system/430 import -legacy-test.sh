22d51cb00e2fd71f007fcf9b5c6ca4cec2b2dbae
Jean Honlet <jehon@users.noreply.github.com>
2024-02-13 08:50:27 +0100

    fix: test 430

--- test/system/430 import -legacy-test.sh
+++ test/system/430 import -legacy-test.sh
@@ -26,7 +26,5 @@ cd "${TO}"
 assert_equals "Number of files imported" "5" "$( find "." -maxdepth 1 -type f | wc -l )"
 assert_equals "Number of files legacy" "3"   "$( find "legacy" -type f | wc -l )"
 
-find .
-
 assert_exists "legacy/2018-01-02 03-04-05 My title [my original name].jpg"
-assert_exists "2024-02-08 22-15-50 1 [ts-guessed].jpg"
+assert_exists "2021-01-04 01-02-03 Vie de famille.jpg"
